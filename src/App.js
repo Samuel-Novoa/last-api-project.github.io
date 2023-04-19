f6da7632db6db28a6fee933331fe56448df1ad97
Samuel Novoa <hmjeronimo272@gmail.com>
2023-04-19 08:09:41 -0500

    New project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Results from "./components/results/Results";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <Results />
+    </div>
+  );
+}
